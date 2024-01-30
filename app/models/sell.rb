902a423c4c459f88ee5ae24094ebb1eddf57410b
Lorenzo Castano <castanolorenzo776@gmail.com>
2024-01-30 00:12:27 -0300

    Making active record and sells controller

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,5 +1,5 @@
 class Sell < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :products
-  validates :total
+  has_and_belongs_to_many :products, dependent: :destroy
+  validates :total, presence: true
 end
