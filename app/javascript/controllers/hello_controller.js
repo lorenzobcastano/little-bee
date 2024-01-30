16921fc3f7c99accf65a48224d4cffa81c6a6eba
Lorenzo Castano <castanolorenzo776@gmail.com>
2024-01-30 15:21:37 -0300

    Added Navbar links

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,16 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 import { jsPDF } from "jspdf";
-
 export default class extends Controller {
   generatePDF(){
     const data = document.getElementById("print")
+    this.doc = new jsPDF("p","px", "a4");
     this.doc.html(data.innerText).save("test.pdf")
   }
   connect() {
 
     // Default export is a4 paper, portrait, using millimeters for units
-    this.doc = new jsPDF("p");
-
     //this.doc.text("Hello world!", 10, 10);
   }
 }
